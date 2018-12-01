e4f02c6791783007fa1ababb7add3ddecee38445
krzysztof83 <k.czechowski83@gmail.com>
2018-12-01 10:55:47 +0100

    Fix CircleCi - @Builder error, fix #6

--- src/main/java/com/czechowski/spring5webfluxrest/domain/Category.java
+++ src/main/java/com/czechowski/spring5webfluxrest/domain/Category.java
@@ -1,7 +1,6 @@
 package com.czechowski.spring5webfluxrest.domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -9,7 +8,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Category {
@@ -18,4 +16,34 @@ public class Category {
     private String id;
 
     private String description;
+
+    public static CategoryBuilder builder() {
+        return new CategoryBuilder();
+    }
+
+    public static class CategoryBuilder {
+        private String id;
+        private String description;
+
+        CategoryBuilder() {
+        }
+
+        public CategoryBuilder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public CategoryBuilder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Category build() {
+            return new Category(id, description);
+        }
+
+        public String toString() {
+            return "Category.CategoryBuilder(id=" + this.id + ", description=" + this.description + ")";
+        }
+    }
 }
